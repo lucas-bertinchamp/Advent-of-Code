83ae05a68b58d8957c57e9f7d5d9368df9e0f2a2
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2022-12-02 10:48:50 +0100

    2022 - day2 - 2 stars

---
+++ 2022/js/days/day2/day2.js
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+
+const array = fs.readFileSync('days/day2/input.txt', 'utf-8').trim().split('\n');
+let score1 = 0;
+let score2 = 0;
+
+array.map((value, index) => {
+    let game = value.split(' ');
+    let player1 = game[0].charCodeAt(0) - 'A'.charCodeAt(0);
+    let player2 = game[1].charCodeAt(0) - 'X'.charCodeAt(0);
+
+    score1 += player2 + 1;
+
+    //Part 1
+    if (!(player2 == 2 && player1 == 0)) {
+        if (player2 == player1) {
+            score1 += 3;
+        } else if (player2 > player1) {
+            score1 += 6;
+        } else if (player2 == 0 && player1 == 2) {
+            score1 += 6;
+        }
+    }
+
+    //Part 2
+    if (player2 == 0) {
+        let point = player1 - 1;
+        if (point == -1) {
+            score2 += 3;
+        } else {
+            score2 += point + 1;
+        }
+    } else if (player2 == 1) {
+        score2 += player1 + 1 + 3;
+    } else {
+        score2 += 6 + ((player1 + 1) % 3) + 1;
+    }
+});
+
+console.log(score1);
+console.log(score2);
