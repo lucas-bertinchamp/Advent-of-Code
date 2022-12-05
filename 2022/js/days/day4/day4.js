0ba7abbe1c84368fe6820b54c06c9186bc498097
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2022-12-05 21:59:01 +0100

    2022 - day 4 - 2 stars

---
+++ 2022/js/days/day4/day4.js
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+const array = fs.readFileSync('days/day4/input.txt', 'utf-8').trim().split('\n');
+
+let answer1 = 0;
+let answer2 = 0;
+
+array.map((value) => {
+    let line = value.split(',');
+    let int1 = line[0].split('-').map(Number);
+    let int2 = line[1].split('-').map(Number);
+
+    //Part 1
+    if (int1[0] <= int2[0] && int2[0] <= int1[1] && int1[0] <= int2[1] && int2[1] <= int1[1]) {
+        answer1 = answer1 + 1;
+    } else if (int2[0] <= int1[0] && int1[0] <= int2[1] && int2[0] <= int1[1] && int1[1] <= int2[1]) {
+        answer1 = answer1 + 1;
+    }
+
+    //Part 2
+    if ((int1[0] <= int2[0] && int2[0] <= int1[1]) || (int1[0] <= int2[1] && int2[1] <= int1[1])) {
+        answer2 = answer2 + 1;
+    } else if ((int2[0] <= int1[0] && int1[0] <= int2[1]) || (int2[0] <= int1[1] && int1[1] <= int2[1])) {
+        answer2 = answer2 + 1;
+    }
+});
+
+console.log(answer1);
+console.log(answer2);
